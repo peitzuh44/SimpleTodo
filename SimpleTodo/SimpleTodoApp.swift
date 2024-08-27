10d8470d148e896a373747a28012675a3f591939
Pei <peitzuh44@berkeley.edu>
2024-08-27 10:24:33 +0800

    Initial Commit

---
+++ SimpleTodo/SimpleTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTodoApp.swift
+//  SimpleTodo
+//
+//  Created by Pei-Tzu Huang on 2024/8/27.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
