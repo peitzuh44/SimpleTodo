b6ce31b4ee91b160dfd305c417e7bc9815121710
Pei <peitzuh44@berkeley.edu>
2024-08-27 11:02:21 +0800

    Complete all UIs and add configure firebase

---
+++ SimpleTodo/Model/Todo.swift
@@ -0,0 +1,8 @@
+//
+//  Todo.swift
+//  SimpleTodo
+//
+//  Created by Pei-Tzu Huang on 2024/8/27.
+//
+
+import Foundation
---
+++ SimpleTodo/View/TodoView.swift
@@ -0,0 +1,18 @@
+//
+//  TodoView.swift
+//  SimpleTodo
+//
+//  Created by Pei-Tzu Huang on 2024/8/27.
+//
+
+import SwiftUI
+
+struct TodoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodoView()
+}
